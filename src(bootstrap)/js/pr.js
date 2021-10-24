7a3e828716b0864518b2e6ad93b390deca3a364f
Dmitry Grizlov <63951854+Dmitry-Grizlov@users.noreply.github.com>
2021-10-24 13:42:37 +0300

    Add files via upload

--- src(bootstrap)/js/pr.js
+++ src(bootstrap)/js/pr.js
@@ -1,3 +1,20 @@
+function Validate() {
+    'use strict'
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+          form.classList.add('was-validated')
+        }, false)
+      })
+};
+
 function GetType() {
 	var select = document.getElementById("type");
 	var value = select.value;
