1cad5114416326e6a2d16612f78e29f3fd2fd76b
Dmitrii Grizlov <dmitry.grizlov20@gmail.com>
2024-10-13 08:10:20 -0700

    Updated the code to close the alert instead of removing it from DOM

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -1,19 +1,19 @@
 function showAlert(message, type = "success") {
-  // Get the alert element by its ID
   const alertDiv = document.getElementById("customAlert");
   const alertMessage = document.getElementById("alertMessage");
 
-  // Update the alert message text
   alertMessage.textContent = message;
-
-  // Update the alert type (class)
   alertDiv.className = `alert alert-${type} alert-dismissible fade show`;
-
-  // Programmatically show the alert
   alertDiv.classList.add("show");
 
-  // Optionally, auto-dismiss the alert after 5 seconds
   setTimeout(() => {
-    $(alertDiv).alert("close");
+    closeAlert();
   }, 5000);
+}
+
+function closeAlert() {
+  const alertDiv = document.getElementById("customAlert");
+  if (alertDiv) {
+    alertDiv.classList.remove("show");
+  }
 }
